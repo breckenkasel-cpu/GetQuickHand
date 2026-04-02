1e9230bd442bec1673c72510910114d93d17a13a
Quintoshagnealius <quintoshagnealius@Quintoshagnealiuss-MacBook-Pro-2.local>
2026-04-02 11:47:48 -0400

    Add Supabase database and auth

--- app/supabase.ts
+++ app/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://jhsuoikzwhkrguawhlyz.supabase.co'
-const supabaseKey = 'sb_publishable_rCsGJ-tQWjOTqg2vgxJ-2A_ATOEDwji'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Impoc3VvaWt6d2hrcmd1YXdobHl6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzUxMzkyODEsImV4cCI6MjA5MDcxNTI4MX0.ifi4B7-9Ht356lvQlNBsxvroXMJzWeC0eGJ1VOQ-Xwg'
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
